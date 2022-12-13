a4acc8a61973d24b65795e2e3fcbf2baeca3be48
tttd16 <tiendung.work16@gmail.com>
2022-12-13 18:26:47 +0700

    update server

---
+++ server/routes/userRouter.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { register, login, updateProfile } from "../controllers/user.js";
+import  auth  from "../middleware/auth.js";
+
+const userRouter = Router();
+userRouter.post("/register", register);
+userRouter.post("/login", login);
+userRouter.patch("/updateProfile", auth, updateProfile);
+
+export default userRouter;
